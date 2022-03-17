963608785a70cf216f2e36fbc4b30009e6943a75
Pivtrek <piotrek.bijacz.7@gmail.com>
2022-03-17 10:50:03 +0100

    Reading euc2d function using numpy library

--- readFile.py
+++ readFile.py
@@ -1,6 +1,14 @@
 import numpy as np
 
-
 def readFullMatrix(name):
     matrix = np.loadtxt(name)
     return matrix
+
+name = "berlin52.tsp"
+
+def readEuc2d(name):
+    euc2d = np.loadtxt(name, usecols=np.arange(1,3))
+    return euc2d
+
+
+print(readEuc2d(name))
